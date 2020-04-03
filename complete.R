4f0d9b215d9110d5e7a46d7201427fdb73445161
Colin Loh <colinloh@hotmail.com>
2020-04-03 10:58:20 -0700

    Got Q2 going

--- complete.R
+++ complete.R
@@ -2,9 +2,18 @@ complete <- function(directory, id=1:332) {
     source("colinslib.R")
     
     population <- getStationValues(directory, id)
-    
-    # Turn the first column into a factor
-    histogram <- as.factor(population[1])
-    setwd(origd)
-    histogram
+    # Init a data frame with the keys and NA for counts.
+    df <- data.frame(id=id, nobs=rep(NA, length(id)))
+    # Update the counts for each key
+    for (station_id in id) {
+        station_obs <- (population[
+            population$ID == station_id & 
+            !is.na(population$sulfate) &
+            !is.na(population$nitrate),
+            ])
+        count <- length(as.vector(attr(station_obs, "row.names")))
+        df[df$id==station_id, 2] <- count
+        #print(paste(station_id, "has", count, "observations"))
+    }
+    df
 }
--- pollutantmean.R
+++ pollutantmean.R
@@ -7,6 +7,6 @@ pollutantmean <- function(directory, pollutant, id=1:332) {
     # From those values, get the ones that have pollutant values (not NA)
     valid_pollutant_vals <- as.array(!is.na(station_observations[[pollutant]]))
     population <- station_observations[valid_pollutant_vals,]
-    print(paste("Found", length(valid_pollutant_vals[valid_pollutant_vals == 1]), "applicable observations out of", length(attr(station_observations,"row.names"))))
+    #print(paste("Found", length(valid_pollutant_vals[valid_pollutant_vals == 1]), "applicable observations out of", length(attr(station_observations,"row.names"))))
     mean(population[[pollutant]])
 }
